3e4bd91e1f8153febab279aae0d98a9cfbfcd88a
Priel <prielhaim7485@gmail.com>
2022-07-07 22:08:30 +0300

    test my project

---
+++ src/test/java/Tests/TestToTest.java
@@ -0,0 +1,12 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class TestToTest {
+
+    @Test(description = "i test the project")
+    public void testing() {
+        System.out.println("test my project");
+
+    }
+}
