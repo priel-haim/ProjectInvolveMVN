1746607a3fc06a40f9a656fef313a8569c7525d4
Priel <prielhaim7485@gmail.com>
2022-07-23 11:34:41 +0300

    add class "TestLoginSite"

---
+++ src/test/java/Tests/TestLoginSite.java
@@ -0,0 +1,37 @@
+package Tests;
+
+import PageObject.LoginPage;
+import PageObject.MyMenu;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestLoginSite extends BaseTest {
+
+	@Test(description = " open my login")
+	public void tc01_loginSite() {
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.cookies();
+		loginPage.openLogin();
+		loginPage.loginToTheSite("prili2@walla.com", "Haim-Haim");
+		String expected = "Haim-a";
+		Assert.assertEquals("Haim-a", expected);
+	}
+
+	@Test(description = "Check if there is a possibility from the user")
+	public void tc02_signOut() {
+		MyMenu yourMenu = new MyMenu(driver);
+		yourMenu.userMenu("Log Out");
+		String condition1 = "logout";
+		driver.get("https://app.involve.me/login");
+		Assert.assertTrue(driver.getTitle() != condition1);
+	}
+
+	@Test(description = "If it is possible to change the user password")
+	public void tc03_forgetMyPassword() {
+		MyMenu yourMenu = new MyMenu(driver);
+		yourMenu.forgetPassword("prili2@walla.com");
+		String condition1 = "Forgot password";
+		driver.get("https://app.involve.me/password/reset");
+		Assert.assertTrue(condition1 != driver.getTitle());
+	}
+}
